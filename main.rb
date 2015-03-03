de9f6a553ac74f14fea060875e6837939cef933e
Melle Zwerver <mellezwerver@outlook.com>
2015-03-03 11:26:16 +0100

    added limit for adding cards (21 points)

--- main.rb
+++ main.rb
@@ -16,15 +16,25 @@ def start_game
 end
 
 def add_cards
-	puts 'another card?'
+	if @player.player_score <= 21
+		puts 'another card?'
 
-	if gets.chomp == 'yes'
-		@player.new_card
+		if gets.chomp == 'yes'
+			@player.new_card
+			score
+		end
+	else puts 'test'
 	end
+end
 
+def score
+	puts
+	puts @player.name + ','
+	puts 'You\'ve now got:'
 	puts @player.player_deck
-
 	puts @player.player_score
+	puts
+	add_cards
 end
 
 test
